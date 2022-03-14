e81530f10fcb4a5ee1ce88a506700ed077fd7716
quanzhou li <qzdy369@vip.qq.com>
2022-03-14 05:58:01 -0700

    update

--- train_mnet_1frame.py
+++ train_mnet_1frame.py
@@ -20,7 +20,7 @@ if __name__=='__main__':
     parser.add_argument('--expr-ID', default='V00', type=str,
                         help='Training ID')
 
-    parser.add_argument('--batch-size', default=256, type=int,
+    parser.add_argument('--batch-size', default=128, type=int,
                         help='Training batch size')
 
     parser.add_argument('--n-workers', default=8, type=int,
