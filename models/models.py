c08ae36fa7374fbc1f3c6c4b5348efc41d3f47ff
quanzhou li <qzdy369@vip.qq.com>
2022-03-14 05:02:09 -0700

    add motion net

--- models/models.py
+++ models/models.py
@@ -194,3 +194,159 @@ class MNet(nn.Module):
                 'future_transl_delta': future_transl.reshape(bs, 10, 3),
                 'future_dists_delta': future_dists.reshape(bs, 10, 99, 3),
                 'future_verts_delta': future_verts.reshape(bs, 10, 400, 3)}
+
+
+# Code adapted from https://github.com/mohamedhassanmus/SAMP_Training/blob/main/src/SAMP_models.py
+
+class INet(nn.Module):
+    def __init__(self):
+        super(INet, self).__init__()
+        self.I = nn.Sequential(
+            nn.Linear(1027, 256),
+            nn.ELU(),
+            nn.Linear(256, 256),
+            nn.ELU(),
+            nn.Linear(256, 256),
+            nn.ELU()
+        )
+
+    def forward(self, I):
+        return self.I(I)
+
+
+class GatingNetwork(nn.Module):
+    def __init__(self, input_size=1830, output_size=512, hidden_size=512):
+        super(GatingNetwork, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+
+        self.gating_network = nn.Sequential(
+            nn.Linear(self.input_size, self.hidden_size),
+            nn.ELU(),
+            nn.Linear(self.hidden_size, self.hidden_size // 2),
+            nn.ELU(),
+            nn.Linear(self.hidden_size // 2, self.output_size),
+            torch.nn.Softmax(dim=1)
+        )
+
+    def forward(self, feature):
+        return self.gating_network(feature)
+
+
+class PredictionNet(nn.Module):
+    def __init__(self, rng, num_experts=6, input_size=1830+256, hidden_size=512, output_size=512, use_cuda=True):
+        super(PredictionNet, self).__init__()
+        self.rng = rng
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.num_experts = num_experts
+        self.use_cuda = use_cuda
+
+        self.w_l1, self.b_l1 = self.init_params(self.num_experts, self.input_size, self.hidden_size)
+        self.w_l2, self.b_l2 = self.init_params(self.num_experts, self.hidden_size, self.hidden_size)
+        self.w_l3, self.b_l3 = self.init_params(self.num_experts, self.hidden_size, self.output_size)
+
+    def init_params(self, num_experts, input_size, output_size):
+        w_bound = np.sqrt(6. / np.prod([input_size, output_size]))
+        w = np.asarray(
+            self.rng.uniform(low=-w_bound, high=w_bound,
+                             size=[num_experts, input_size, output_size]),
+                             dtype=np.float32
+        )
+        if self.use_cuda:
+            w = torch.nn.Parameter(
+                torch.cuda.FloatTensor(w), requires_grad=True)
+            b = torch.nn.Parameter(
+                torch.cuda.FloatTensor(num_experts, output_size).fill_(0),
+                requires_grad=True)
+        else:
+            w = torch.nn.Parameter(
+                torch.FloatTensor(w), requires_grad=True)
+            b = torch.nn.Parameter(
+                torch.FloatTensor(num_experts, output_size).fill_(0),
+                requires_grad=True)
+        return w, b
+
+    def dropout_and_linearlayer(self, inputs, weights, bias):
+        return torch.sum(inputs[..., None] * weights, dim=1) + bias
+
+    def forward(self, frame_prev, I, blending_coef):
+        w_l1 = torch.sum(
+            blending_coef[..., None, None] * self.w_l1[None], dim=1)
+        b_l1 = torch.matmul(blending_coef, self.b_l1)
+
+        w_l2 = torch.sum(
+            blending_coef[..., None, None] * self.w_l2[None], dim=1)
+        b_l2 = torch.matmul(blending_coef, self.b_l2)
+
+        w_l3 = torch.sum(
+            blending_coef[..., None, None] * self.w_l3[None], dim=1)
+        b_l3 = torch.matmul(blending_coef, self.b_l3)
+
+        h0 = torch.concat([frame_prev, I], dim=1)
+
+        h1 = F.elu(self.dropout_and_linearlayer(h0, w_l1, b_l1))
+        h2 = F.elu(self.dropout_and_linearlayer(h1, w_l2, b_l2))
+        h3 = F.elu(self.dropout_and_linearlayer(h2, w_l3, b_l3))
+
+        return h3
+
+
+class MotionNet(nn.Module):
+    def __init__(self, rng=None, num_experts=6, use_cuda=True):
+        super(MotionNet, self).__init__()
+        self.INet = INet()
+        self.gating_network = GatingNetwork(output_size=num_experts)
+        self.rng = rng
+        self.prediction_net = PredictionNet(self.rng, num_experts=num_experts, use_cuda=use_cuda)
+
+        self.fc_pose = nn.Linear(512, 55*6)
+        self.fc_transl = nn.Linear(512, 3)
+        self.fc_verts = nn.Linear(512, 400*3)
+        self.fc_dists = nn.Linear(512, 99*3)
+
+    def forward(self, past_pose_rotmat, past_transl, cur_verts, cur_dists_to_goal, bps_goal, obj_goal_transl, **kwargs):
+        '''
+        :param past_pose_rotmat: (N, 1, 55, 9)
+        :param past_transl: (N, 3)
+        :param cur_velocity: (N, 400, 3)
+        :param cur_verts: (N, 400, 3)
+        :param cur_dists_to_goal: (N, 99, 3)
+        :param bps_goal: (N, 1024)
+        :param obj_goal_transl: (N, 3)
+        :param kwargs:
+        :return:
+        '''
+        bs = past_pose_rotmat.shape[0]
+
+        pose_6D = (past_pose_rotmat.reshape(bs, 1, 55, 3, 3))[:,:,:,:,:2]
+        pose_6D = pose_6D.reshape(bs, 55*6)
+        frame_prev = torch.concat([pose_6D, past_transl, cur_verts.flatten(start_dim=1), cur_dists_to_goal.flatten(start_dim=1)], dim=1)
+        I = torch.concat([bps_goal, obj_goal_transl.to(torch.float32)], dim=1)
+
+        I = self.INet(I)
+        omega = self.gating_network(frame_prev)
+        Y = self.prediction_net(frame_prev, I, omega)
+
+        future_pose_6D = self.fc_pose(Y)
+        future_transl = self.fc_transl(Y)
+        future_verts = self.fc_verts(Y)
+        future_dists = self.fc_dists(Y)
+
+        future_pose_rotmat = future_pose_6D.reshape(bs, 1, 55 * 6)
+        future_pose_rotmat = CRot2rotmat(future_pose_rotmat).reshape(bs, 1, 55, 9)
+
+        return {
+            'future_pose': future_pose_rotmat,
+            'future_transl': future_transl,
+            'future_dists': future_dists.reshape(bs, 99, 3),
+            'future_verts': future_verts.reshape(bs, 400, 3)
+        }
+
+
+
+
+
+
