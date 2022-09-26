6c42b26f1cc4ddb03dc58cc08a538aea92025ae2
quanzhou li <qzdy369@vip.qq.com>
2022-09-26 16:50:08 -0700

    update

--- optim/gnet_optim.py
+++ optim/gnet_optim.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 from tools.utils import makepath, to_cpu, to_np, to_tensor
-from tools.utils import aa2rotmat, rotmat2aa
+from tools.utils import aa2rotmat, rotmat2aa, CRot2rotmat
 from bps_torch.bps import bps_torch
 import chamfer_distance as chd
 
@@ -32,26 +32,27 @@ class GNetOptim(nn.Module):
         device = self.device
         dtype = self.dtype
         self.opt_params = {
-            'global_orient': torch.randn(bs, 1 * 3, device=device, dtype=dtype, requires_grad=True),
-            'body_pose': torch.randn(bs, 21 * 3, device=device, dtype=dtype, requires_grad=True),
-            'left_hand_pose': torch.randn(bs, 15 * 3, device=device, dtype=dtype, requires_grad=True),
-            'right_hand_pose': torch.randn(bs, 15 * 3, device=device, dtype=dtype, requires_grad=True),
-            'jaw_pose': torch.randn(bs, 1 * 3, device=device, dtype=dtype, requires_grad=True),
-            'leye_pose': torch.randn(bs, 1 * 3, device=device, dtype=dtype, requires_grad=True),
-            'reye_pose': torch.randn(bs, 1 * 3, device=device, dtype=dtype, requires_grad=True),
+            'global_orient': torch.randn(bs, 1 * 3 * 2, device=device, dtype=dtype, requires_grad=True),
+            'body_pose': torch.randn(bs, 21 * 3 * 2, device=device, dtype=dtype, requires_grad=True),
+            'left_hand_pose': torch.randn(bs, 15 * 3 * 2, device=device, dtype=dtype, requires_grad=True),
+            'right_hand_pose': torch.randn(bs, 15 * 3 * 2, device=device, dtype=dtype, requires_grad=True),
+            'jaw_pose': torch.randn(bs, 1 * 3 * 2, device=device, dtype=dtype, requires_grad=True),
+            'leye_pose': torch.randn(bs, 1 * 3 * 2, device=device, dtype=dtype, requires_grad=True),
+            'reye_pose': torch.randn(bs, 1 * 3 * 2, device=device, dtype=dtype, requires_grad=True),
             'transl': torch.zeros(bs, 3, device=device, dtype=dtype, requires_grad=True),
         }
 
-        self.opt_s3 = optim.Adam([self.opt_params[k] for k in ['global_orient', 'transl', 'right_hand_pose']], lr=self.cfg.lr)
+        self.opt_s3 = optim.Adam([self.opt_params[k] for k in ['global_orient', 'transl', 'body_pose', 'right_hand_pose']], lr=self.cfg.lr)
         self.optimizer = [self.opt_s3]
         self.num_iters = [self.cfg.n_iters]
 
         self.LossL1 = nn.L1Loss(reduction='mean')
 
     def init_params(self, start_params):
-        self.sbj_params = start_params
+        self.sbj_params_6D = {}
         for k in self.opt_params.keys():
-            self.opt_params[k].data = torch.repeat_interleave(start_params[k], 1, dim=0)
+            self.sbj_params_6D[k] = aa2rotmat(start_params[k]).reshape(-1, 3, 3)[:, :, :2].reshape(-1, 6) if k != 'transl' else start_params[k]
+            self.opt_params[k].data = torch.repeat_interleave(self.sbj_params_6D[k], 1, dim=0)
 
     def get_smplx_verts(self, net_output, obj_params):
         obj_output = self.obj_m(**obj_params)
@@ -80,10 +81,14 @@ class GNetOptim(nn.Module):
         return sbj_params
 
     def calc_loss(self, net_output):
-        opt_params = {k:aa2rotmat(v) for k,v in self.opt_params.items() if k!='transl'}
-        opt_params['transl'] = self.opt_params['transl']
+        opt_params_rotmat = {}
+        opt_params = {}
+        for k in self.opt_params.keys():
+            if k != 'transl':
+                opt_params_rotmat[k] = CRot2rotmat(self.opt_params[k]).reshape(-1, 9)
+                opt_params[k] = rotmat2aa(opt_params_rotmat[k]).reshape(-1, 3)
 
-        output = self.sbj_m(**self.opt_params, return_full_pose=True)
+        output = self.sbj_m(**opt_params, return_full_pose=True)
         verts = output.vertices
         verts_rh = verts[:, self.rhand_idx]
 
